097cdffd1a76a82d9acde99d1574c2981207f895
Jairaj Choudhary <choudharyjairaj569@gmail.com>
2026-04-24 22:14:04 +0530

    Add files via upload

---
+++ precautions.py
@@ -0,0 +1,5 @@
+precautions = {
+    "Tomato Early Blight": "Remove infected leaves, use fungicide, avoid overhead watering.",
+    "Tomato Late Blight": "Use resistant varieties, apply copper-based fungicide.",
+    "Healthy": "No disease detected. Maintain proper watering and sunlight."
+}
